a215a474282a09244a515252979f2b5dc4779031
Jual Rana <contactjual@gmail.com>
2024-11-29 21:31:22 +0600

    coding pop up commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const aiData = async () => {
     const array = Object.values(ai)
 
     // console.log(ai)
-    // console.log(array)
+    console.log(array)
 
     displayAiInfo(array);
 
@@ -18,48 +18,114 @@ const aiData = async () => {
 // display ai info 
 const displayAiInfo = (array) => {
 
-    array.forEach(singleAi => {
-        console.log(singleAi);
-    
-        // add card 
-    
-        const cardSection = document.getElementById('card-section');
-    
-        cardSection.innerHTML = `
-            <div onclick="showPopup()"  class="card">
-                <div class="features">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_1j5EelHEOtoLagpQtbwPZdrztpEQL0sJLA&s"
-                        alt="">
-                    <h2>Features</h2>
-                    <p>1. Natural language processing</p>
-                    <p>2. Contextual understanding</p>
-                    <p>3. Text generation</p>
-                </div>
-                <hr>
-                <div class="foot">
-                    <div class="name-date">
-                        <h2>${singleAi[0].name}</h2>
-                        <i class="fa-solid fa-calendar-days"></i>
-                        <span>11/01/2022</span>
+    array.forEach(allAi => {
+
+        allAi.forEach(singleAi => {
+
+            // console.log(singleAi.features);
+
+            // add card 
+            const cardSection = document.getElementById('card-section');
+
+            const creatDiv = document.createElement('div');
+
+            creatDiv.innerHTML = `
+                <div onclick="showPopup()"  class="card">
+                    <div class="features">
+                        <img src="${singleAi.image}"
+                            alt="">
+                        <h2>Features</h2>
+                        <p>1. ${singleAi.features[0]}</p>
+                        <p>2. ${singleAi.features[1]}</p>
+                        <p>3. ${singleAi.features[2]}</p>
                     </div>
-                    <i class="fa-solid fa-arrow-right"></i>
+                    <hr>
+                    <div class="foot">
+                        <div class="name-date">
+                            <h2>${singleAi.name}</h2>
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <span>${singleAi.published_in}</span>
+                        </div>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </div>
                 </div>
-            </div>
-        `;
+            `;
+
+            cardSection.appendChild(creatDiv);
+
+            showPopup(singleAi);
+
+
+            });
+
     });
 }
-    
+
 
 
 aiData();
 
 
 
-
 // show popup
 
-const showPopup = () => {
+const showPopup = (singleAi) => {
+
+
+    // array.forEach(allAi => {
+
+    //     allAi.forEach(singleAi =>{
+
+    //         console.log(singleAi)
+    //     });
+
+    // });
+
+
     const overPop = document.getElementById('over-pop');
+
+    // create popup
+    overPop.innerHTML = `
+        <div class="pops">
+                <div class="pop-up">
+                    <div class="left-card">
+                        <h2>ChatGPT is an AI-powered chatbot platform that uses OpenAI's GPT technology to simulate
+                            human
+                            conversation.</h2>
+                        <div class="mini-card">
+                            <span class="basic">$10/ <br> month <br> Basic</span>
+                            <span class="pro">$50/ <br> month <br> Pro</span>
+                            <span class="enterprise">Contact <br> us <br> Enterprise</span>
+                        </div>
+                        <div class="foot">
+                            <div class="left">
+                                <h3>Features</h3>
+                                <ul>
+                                    <li>Customizable responses</li>
+                                    <li>Multilingual support</li>
+                                    <li>Seamless integration</li>
+                                </ul>
+                            </div>
+                            <div class="right">
+                                <h3>Integrations</h3>
+                                <ul>
+                                    <li>FB Messenger</li>
+                                    <li>Slack</li>
+                                    <li>Telegram</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right-card">
+                        <img src="${singleAi.image}" alt="">
+                        <h2>Hi, how are you doing today?</h2>
+                        <p>I'm doing well, thank you for asking. How can I assist you today?</p>
+                    </div>
+                </div>
+            </div>
+            <i onclick="closePopup()" class="fa-solid fa-xmark"></i>
+    `;
+
     overPop.style.display = 'flex';
 
     // const blockAll = document.getElementById('block-all');
@@ -76,4 +142,5 @@ const closePopup = () => {
 
     // const blockAll = document.getElementById('block-all');
     // blockAll.remove.classList = ('block');
-}
+};
+
